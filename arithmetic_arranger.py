2b69b1437990d5b74e88ca2329f01f453f734765
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-14 05:48:01 +0100

    refactor: add solution to find answer

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,6 +5,7 @@ def arithmetic_arranger(equations, flag):
     a = ''
     b = ''
     _ = ''
+    answer = ''
     for equation in equations:
         first_n, operator, second_n = equation.split(' ')
         a += first_n.rjust(5).ljust(13)
@@ -12,13 +13,16 @@ def arithmetic_arranger(equations, flag):
         b += operator.ljust(3)
         b += second_n.ljust(10)
         _ += '-------'.rjust(3).ljust(13)
+        if flag:
+            answer += str(eval(equation)).rjust(5).ljust(13)
 ##        print(b)
 
     print(a)
     print(b)
     print(_)
+    print(answer)
 
 
 
-arithmetic_arranger(['30 + 5', '30 + 5', '30 + 5', '30 + 5', '30 + 5'],False)
+arithmetic_arranger(['30 + 5', '30 + 5', '30 + 5', '30 + 5', '30 + 5'],True)
 
