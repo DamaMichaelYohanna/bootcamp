362de7184d9508e5dfcd12f99faa49436bf2a0a6
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-14 05:44:28 +0100

    refactor: added flag

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,21 +1,24 @@
-def arithmetic_arranger(equations):
+def arithmetic_arranger(equations, flag):
     """this takes list of arithmetic problem and rearrange them"""
     if len(equations) > 5:
         return "To many Problems"
     a = ''
     b = ''
+    _ = ''
     for equation in equations:
         first_n, operator, second_n = equation.split(' ')
-        a += first_n.rjust(5)
+        a += first_n.rjust(5).ljust(13)
 ##        print(a)
         b += operator.ljust(3)
         b += second_n.ljust(10)
+        _ += '-------'.rjust(3).ljust(13)
 ##        print(b)
 
     print(a)
     print(b)
+    print(_)
 
 
 
-arithmetic_arranger(['30 + 5', '30 + 5', '30 + 5', '30 + 5', '30 + 5'])
+arithmetic_arranger(['30 + 5', '30 + 5', '30 + 5', '30 + 5', '30 + 5'],False)
 
